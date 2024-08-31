ea2baf44616916d838cddefdfcf8b7cf3efcc923
Carlos0550 <carlospelinski05@gmail.com>
2024-08-31 12:20:25 -0300

    componente para proteger rutas

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,12 +1,12 @@
-import { Navigate } from "react-router-dom";
-import { useAppContext } from "../context";
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { useAppContext } from '../context';
 
 export function ProtectedRoute({ children }) {
   const { isAuthenticated } = useAppContext();
-
+  console.log("Ejecuta el protector de rutas")
   if (isAuthenticated === false) {
     return <Navigate to="/" replace />;
   }
-
   return children;
 }
